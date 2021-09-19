73e18abe2201a29cb129723a41bde7f45a1ab358
sebastian <tjonycage@gmail.com>
2021-09-19 20:11:24 -0300

    forms

--- src/app/pages/inputs/inputs.page.ts
+++ src/app/pages/inputs/inputs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inputs',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputsPage implements OnInit {
 
-  constructor() { }
+
+  n1: number;
+  n2: number;
+
+  x: number;
+  y: number;
+
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async onClick() {
+
+    // eslint-disable-next-line prefer-const
+    let res= this.n1+this.n2;
+    const alert = await this.alertController.create({
+      header: 'Resulta de la suma',
+      message: res.toString(),
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async sumarForm()
+  {
+    const alert = await this.alertController.create({
+      header: 'Resulta de la suma con Formulario',
+      message: (this.x+this.y).toString(),
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
 }
